a29dc79acfd051c222cda9ae6aaf57d526c41757
Aditya Chaurasia <t.aditya.chaurasia@e-arc.com>
2025-04-25 12:12:00 +0530

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Aditya Chaurasia on 25/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
